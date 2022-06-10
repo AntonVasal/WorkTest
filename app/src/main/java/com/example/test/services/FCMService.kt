659a9518677f1e0a1c523163f7bfb55f69ff2dca
Anton <avasalatij@gmail.com>
2022-06-10 16:53:03 +0300

    1.9 Summer June Day 10

--- app/src/main/java/com/example/test/services/FCMService.kt
+++ app/src/main/java/com/example/test/services/FCMService.kt
@@ -1,20 +1,68 @@
 package com.example.test.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.test.R
 import com.example.test.domain.models.MainUserModel
+import com.example.test.ui.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.greenrobot.eventbus.EventBus
 
 class FCMService : FirebaseMessagingService() {
 
+    private val NOTIFICATION_CHANNEL_ID = "notification"
+    private val NOTIFICATION_ID = 100
+
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        Log.i("FCM", token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         val id = message.data["id"]?.toInt() ?: 0
-        val changesCount = message.data["changesCount"]?.toInt()?:0
+        val changesCount = message.data["changesCount"]?.toInt() ?: 0
         EventBus.getDefault().postSticky(MainUserModel(id, changesCount))
+
+        val title = message.notification!!.title
+        val body = message.notification!!.body
+        showNotification(applicationContext, title, body)
     }
+
+    private fun showNotification(context: Context, title: String?, message: String?) {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        val notification: Notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setOngoing(true)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setWhen(System.currentTimeMillis())
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setContentTitle(title).build()
+        val notificationManager = context.getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+        val notificationChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            title,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
 }
